769720008ac92df0c0907773eff99706a15d0459
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2019-05-19 14:07:31 +0100

    implement simple map method

--- src/observable.js
+++ src/observable.js
@@ -72,6 +72,26 @@ export default class Observable {
     });
   }
 
+  static map(callback) {
+    return new Observable(observer => {
+      const subscription = this.subscribe({
+        next(value) {
+          observer.next(callback(value));
+        },
+
+        error(fail) {
+          observer.error(fail);
+        }
+      });
+
+      return {
+        unsubscribe() {
+          subscription.unsubscribe();
+        }
+      };
+    });
+  }
+
   static concat(..._observables) {
     return new Observable(observer => {
       let observables = [..._observables];
